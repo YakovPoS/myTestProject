90e331536546ca52194ef1489069eb192a94c1f3
YakovPoS <yakovtoupersev@gmail.com>
2023-05-19 17:03:17 +0500

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+
+app.get('/',(req,res)=>{
+    res.send(path.join(__dirname, 'views', 'index.html'))
+})
+console.log(path.join(__dirname, 'views', 'index.html'))
+
+
+const port = 3000
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port} !`)    
+})
